a3062f4d38a76226f17c6e76954c91d6b1c27296
Tymoteusz Stępkowski <65712281+tymowskyy@users.noreply.github.com>
2025-05-17 08:27:09 +0000

    editor: backspace

--- editor.asm
+++ editor.asm
@@ -4,6 +4,7 @@ section .data
     input_char      db 0
     default_str    db 'xxxxxxxxxxxxxxxxxxxx', 10,  0
     ;default_str    db '                    ', 10,  0
+    nl db 10, 0
 
     cursor_col    dd 0            ; Column within that line
     show_cursor db 0x1B, '[?25h', 0  ;
@@ -15,6 +16,7 @@ section .data
     home db 27, '[H', 0
     save_cursor db 27, '[s', 0
     restore_cursor db 27, '[u', 0
+    clear_line db 27, '[2K', 0
     
 section .bss
     lines_buffer  resb 1024 * 22
@@ -108,8 +110,13 @@ _start:
     mov rdx, 1
     syscall
 
+    cmp byte [input_char], '`'
+    je .backspace
+    cmp byte [input_char], 9
+    je .restore_and_exit
     cmp byte [input_char], 0x1B     ; ESC
     jne .handle_char
+
 
     ; Read '['
     mov rax, 0
@@ -181,20 +188,67 @@ _start:
 
     inc dword [current_line]
     jmp .loop_input
+.backspace:
+    mov ecx, [current_line]
+
+    dec dword [line_lengths + 4*rcx]
+    dec dword [cursor_col]
+    mov edx, [cursor_col]
+    mov edi, [line_lengths + 4*rcx]
+    mov rax, [line_pointers + 8*rcx]
+
+
+    cmp rdi, rdx
+    jle .pushedleft
+    .loop_pushleft:
+    mov r8b, [rax + rdx + 1]
+    mov byte [rax + rdx], r8b
+    inc rdx
+    cmp rdi, rdx
+    jg .loop_pushleft
+    .pushedleft:
+
+    mov rax, 1                        ; sys_write syscall
+    mov rdi, 1                        ; STDOUT (file descriptor 1)
+    mov rsi, left             ; Address of the input character
+    mov rdx, 3                        ; Length of the character (1 byte)
+    syscall 
+    jmp .print_lines
 
 .handle_char:
     ; Handle normal characters here (store to buffer, print, etc.)
     ; Example:
-    mov rcx, [current_line]
+    mov ecx, [current_line]
     mov edx, [cursor_col]
+
+    mov edi, [line_lengths + 4*rcx]
+    mov rax, [line_pointers + 8*rcx]
+    dec rdi
+
+    cmp rdi, rdx
+    jl .pushedright
+.loop_pushright:
+    mov r8b, [rax + rdi]
+    mov byte [rax + rdi + 1], r8b
+    dec rdi
+    cmp rdi, rdx
+    jge .loop_pushright
+.pushedright:
+
+
+
+    inc dword [line_lengths + 4*rcx]
+
     movzx rdi, byte [input_char]
     mov rax, [line_pointers + 8*rcx]
     mov byte [rax + rdx], dil
     inc dword [cursor_col]
 
+
+.print_lines:
     mov rax, 1                        ; sys_write syscall
     mov rdi, 1                        ; STDOUT (file descriptor 1)
-    mov rsi, save_cursor             ; Address of the input character
+    mov rsi, save_cursor             ; Address of the inputf character
     mov rdx, 3                       ; Length of the character (1 byte)
     syscall    
     mov rax, 1              ; sys_write
@@ -202,26 +256,40 @@ _start:
     mov rsi, home    ; pointer to the string
     mov rdx, 3              ; length of the ANSI code
     syscall
-.print_lines:
 
     mov r8, 0
 .print_loop:
+    mov rax, 1                        ; sys_write syscall
+    mov rdi, 1                        ; STDOUT (file descriptor 1)
+    mov rsi, clear_line             ; Address of the input character
+    mov rdx, 4                        ; Length of the character (1 byte)
+    syscall
+
     mov rax, 1
     mov rdi, 1
     mov rsi, [line_pointers + 8*r8]
-    mov rdx, 21
+    mov edx, [line_lengths + 4*r8]
+    syscall
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, nl
+    mov rdx, 1
     syscall
     inc r8
     cmp r8, 4
     jl .print_loop
 
 
-        mov rax, 1                        ; sys_write syscall
+    mov rax, 1                        ; sys_write syscall
     mov rdi, 1                        ; STDOUT (file descriptor 1)
     mov rsi, restore_cursor             ; Address of the input character
     mov rdx, 3                        ; Length of the character (1 byte)
-    syscall    
-        mov rax, 1                        ; sys_write syscall
+    syscall
+    
+    cmp byte [input_char], '`'
+    je .loop_input
+
+    mov rax, 1                        ; sys_write syscall
     mov rdi, 1                        ; STDOUT (file descriptor 1)
     mov rsi, right             ; Address of the input character
     mov rdx, 3                        ; Length of the character (1 byte)
